4951cf0901b6f027cc022d172679449ec22ce63e
gerry883 <60855680+gerry883@users.noreply.github.com>
2024-12-17 12:27:24 +0000

    Update __init__.py

--- mlfinlab/microstructural_features/__init__.py
+++ mlfinlab/microstructural_features/__init__.py
@@ -7,7 +7,7 @@ from mlfinlab.microstructural_features.encoding import quantile_mapping, encode_
 from mlfinlab.microstructural_features.entropy import get_lempel_ziv_entropy, get_shannon_entropy, get_plug_in_entropy, \
     get_konto_entropy
 from mlfinlab.microstructural_features.feature_generator import MicrostructuralFeaturesGenerator
-from mlfinlab.microstructural_features.feature_generator import MicrostructuralFeaturesGeneratorEnhanced
+from mlfinlab.microstructural_features.feature_generator_enhanced import MicrostructuralFeaturesGeneratorEnhanced
 from mlfinlab.microstructural_features.first_generation import get_corwin_schultz_estimator, get_roll_measure, \
     get_roll_impact, get_bekker_parkinson_vol
 from mlfinlab.microstructural_features.misc import get_avg_tick_size, vwap
